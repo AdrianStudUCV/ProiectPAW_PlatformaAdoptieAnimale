a464e0f31d7e01e25c0074ae030ae766d7786b18
Adrian Cherciu <cherciu.dumitru.d2r@student.ucv.ro>
2026-04-17 02:49:13 +0300

    Add files via upload

---
+++ Views/Home/ProfilUtilizator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace modelMVC.Views.Home
+{
+    public class ProfilUtilizatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/Home/adoptie.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace modelMVC.Views.Home
+{
+    public class adoptieModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
